6e0e967d1367d10eb51ac12d61a308a2769267b6
mohomarr <114835274+mohomarr@users.noreply.github.com>
2023-04-09 11:51:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let ALLP = document.getElementById('img-Cc');
+let Black = document.getElementById('blk1');
+let Blue = document.getElementById('blu1');
+let RED = document.getElementById('red');
+let Green = document.getElementById('gren1');
+let Major = document.getElementById('img-C')
+
+ALLP.onclick = function(phone) {
+    document.body.style.backgroundColor = 'limegreen';
+     phone = Major.src = "images/0.jpg" ;
+
+}
+
+Black.onclick = function(phone) {
+    document.body.style.backgroundColor = 'Black';
+     phone = Major.src = this.src ;
+     beige
+}
+
+Blue.onclick = function(phone) {
+    document.body.style.backgroundColor = 'blue';
+     phone = Major.src = this.src ;
+
+
+}
+
+RED.onclick = function(phone) {
+    document.body.style.backgroundColor = 'red';
+     phone = Major.src = this.src ;
+}
+
+Green.onclick = function(phone) {
+    document.body.style.backgroundColor = 'green';
+     phone = Major.src = this.src ;
+
+}
